2fff05c3da2f098dce51825df5da946311ccf46b
Dávid Szabó <davidka1997@gmail.com>
2019-05-14 01:58:06 +0200

    Removed unused stuff

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors'); // https://expressjs.com/en/resources/middleware/cors.html
 
-const indexRouter = require('./routes/index');
-const inventoryRouter = require('./routes/inventory');
-const lentsRouter = require('./routes/lents');
-const membersRouter = require('./routes/members');
 const dbRouter = require('./routes/db');
 
 let app = express();
@@ -22,12 +18,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/inventory', inventoryRouter);
-app.use('/lents', lentsRouter);
-app.use('/members', membersRouter);
 app.use('/db', dbRouter);
 
 module.exports = app;
